35e8c0e96d961c66eb50cf93f6e80aa18403d444
Lucas  Duarte <dolucasduarte@gmail.com>
2021-01-05 16:21:44 -0300

    Auth Pages Styled

--- labeddit/src/styles/globalStyles.js
+++ labeddit/src/styles/globalStyles.js
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
 body {
     margin: 0;
     padding: 0;
-    background: yellow;
+    background: ##F7F7F7;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
