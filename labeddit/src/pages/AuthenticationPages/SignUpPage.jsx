328d215c70f944bc16eadf5553a8e6c5b38ae310
Lucas  Duarte <dolucasduarte@gmail.com>
2021-01-14 17:56:59 -0300

    Sign Up Page Style corrected

--- labeddit/src/pages/AuthenticationPages/SignUpPage.jsx
+++ labeddit/src/pages/AuthenticationPages/SignUpPage.jsx
@@ -1,19 +1,20 @@
 import {
-  PageContainer,
+  AuthenticationPageContainer,
+  FormContainer,
   LogoIcon,
-  AuthenticationForm,
-  AuthenticationFormTitle,
-  AuthenticationCTA
-} from "./AuthPages.style";
+  Title,
+  Form
+} from "./AuthenticationPages.style";
 import {
   UsernameInput,
   EmailInput,
   PasswordInput
 } from "../../components/Inputs/AuthenticationInputs";
-import useForm from "../../hooks/useForm";
-import { signUp } from "../../services/post";
+import useForm from "hooks/useForm";
+import { signUp } from "services/post";
 import { Link, useHistory } from "react-router-dom";
-import logoIcon from "../../images/logo-icon.svg";
+import logoIcon from "images/logo-icon.svg";
+import { Button } from "components/Buttons/Buttons";
 
 function SignUpPage() {
   const history = useHistory();
@@ -35,21 +36,23 @@ function SignUpPage() {
   };
 
   return (
-    <PageContainer>
-      <LogoIcon src={logoIcon} alt="Reddit logo icon" />
-      <AuthenticationFormTitle>
-        <strong>Sign up</strong> and join the community.
-      </AuthenticationFormTitle>
-      <AuthenticationForm onSubmit={submitSignUp}>
-        <UsernameInput onChange={onChange} form={form} />
-        <EmailInput onChange={onChange} form={form} />
-        <PasswordInput onChange={onChange} form={form} />
-        <button>Sign up</button>
-      </AuthenticationForm>
-      <AuthenticationCTA>
-        Already registered? <Link to="/login">Sign in</Link>
-      </AuthenticationCTA>
-    </PageContainer>
+    <AuthenticationPageContainer>
+      <FormContainer>
+        <LogoIcon src={logoIcon} alt="Reddit logo icon" />
+        <Title>
+          <strong>Sign up</strong> and join the community.
+        </Title>
+        <Form onSubmit={submitSignUp}>
+          <UsernameInput onChange={onChange} form={form} />
+          <EmailInput onChange={onChange} form={form} />
+          <PasswordInput onChange={onChange} form={form} />
+          <Button callToAction="Sign Up" />
+        </Form>
+        <p>
+          Already registered? <Link to="/login">Sign in</Link>
+        </p>
+      </FormContainer>
+    </AuthenticationPageContainer>
   );
 }
 
